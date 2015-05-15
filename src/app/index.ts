70b1df6c04ef42b56a535b064e07e1ef67aff20d
Matt <mcritch@vmware.com>
2015-05-15 13:09:05 -0700

    Description:     Initial check-in of code generated by generator-gulp-angular

---
+++ src/app/index.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../.tmp/typings/tsd.d.ts" />
+
+/// <reference path="main/main.controller.ts" />
+/// <reference path="../app/components/navbar/navbar.controller.ts" />
+
+module gulpAngularTypescriptBoilerplate {
+  'use strict';
+
+  angular.module('gulpAngularTypescriptBoilerplate', ['ngCookies', 'ngRoute'])
+    .controller('MainCtrl', MainCtrl)
+    .controller('NavbarCtrl', NavbarCtrl)
+
+  .config(function ($routeProvider: ng.route.IRouteProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
+}
